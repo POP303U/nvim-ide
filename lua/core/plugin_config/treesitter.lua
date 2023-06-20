a276b4c685e34d6709f3c5a254915d3f4fa285ab
POP303U <nicoanton.gering@gmail.com>
2023-06-20 16:08:29 +0200

    turned off auto install

--- lua/core/plugin_config/treesitter.lua
+++ lua/core/plugin_config/treesitter.lua
@@ -4,7 +4,7 @@ require'nvim-treesitter.configs'.setup {
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = true,
-  auto_install = true,
+  auto_install = false,
 
   highlight = {
     enable = true,
