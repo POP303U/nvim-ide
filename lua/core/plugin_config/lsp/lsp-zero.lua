9568d45b943c9a0ed5fd17d1eea0b294417c7034
POP303U <nicoanton.gering@gmail.com>
2023-06-24 22:56:09 +0200

    updated README

--- lua/core/plugin_config/lsp/lsp-zero.lua
+++ lua/core/plugin_config/lsp/lsp-zero.lua
@@ -17,9 +17,9 @@ lsp.set_preferences({
     }
 })
 
-lsp.on_attach(function(client, bufnr)
+lsp.on_attach(function(client, bufnr) --client is unused for some reason
 local opts = {buffer = bufnr, remap = false}
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts) --what is this
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
