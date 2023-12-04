7ee05e2bf28145e951bde2a739334bca4551ce2f
POP303U <nicoanton.gering@gmail.com>
2023-12-04 15:50:34 +0100

    Add: Neogit support

---
+++ lua/core/plugin_config/neogit.lua
@@ -0,0 +1,10 @@
+require("neogit").setup({
+    kind = "split", -- opens neogit in a split
+    signs = {
+        -- { CLOSED, OPENED }
+        section = { "", "" },
+        item = { "", "" },
+        hunk = { "", "" },
+    },
+    integrations = { diffview = true }, -- adds integration with diffview.nvim
+})
